987a26c470ebed1e7a11226e1448de8ed3dc9b33
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2023-08-09 18:49:20 +0900

    improve a resnet18.

--- image_recognition/ch04_04_better_cnn.py
+++ image_recognition/ch04_04_better_cnn.py
@@ -1,7 +1,4 @@
-import copy
-from collections import deque
-
 from image_recognition.utils import cnn_train_evaluation
 
 if __name__ == "__main__":
-    cnn_train_evaluation()
+    cnn_train_evaluation(improved=True)
